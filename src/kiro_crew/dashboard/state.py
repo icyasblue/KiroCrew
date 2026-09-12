34d82ebfaba0157d49f1b5bc2f2cef62d8af0cf4
Nick Bowers <nrb@amazon.com>
2026-09-12 10:54:55 +0000

    feat(slack): one renderer for both ingress kinds, images flow both ways

--- src/kiro_crew/dashboard/state.py
+++ src/kiro_crew/dashboard/state.py
@@ -3489,6 +3489,7 @@ class _ChatSlot:
         *,
         directive_user_origin: bool = False,
         directive_channel_origin: bool = False,
+        ingress: str = "",
     ) -> str:
         return self._queue_repository.queue_append(
             self,
@@ -3497,6 +3498,7 @@ class _ChatSlot:
             meta,
             directive_user_origin=directive_user_origin,
             directive_channel_origin=directive_channel_origin,
+            ingress=ingress,
         )
 
     def _note_enqueue(self) -> None:
